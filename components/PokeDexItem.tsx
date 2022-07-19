917805dcd2419764b68b6104f00ae4af05e0e267
Edgar Klepek <edgarklepek@gmail.com>
2022-07-19 16:10:10 +0200

    Add toggle for checkboxes of PokeDexItems

--- components/PokeDexItem.tsx
+++ components/PokeDexItem.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./PokedexItem.module.css";
+import { useState } from "react";
 
 interface Props {
   id: number;
@@ -12,14 +13,22 @@ const PokeDexItem = ({ id, name, gen, completed }: Props) => {
   const fullId = `${id}`.padStart(3, "0");
   const imgSrc = `/../public/assets/images/pokemon_icon_${fullId}_00.png`;
 
-  const changeCompleted = () => {};
+  const [itemCompleted, setItemCompleted] = useState(completed);
+
+  const toggleCompleted = (completed: boolean) => {
+    setItemCompleted(!completed);
+  };
 
   return (
     <div className={styles["pokedex-item"]}>
       <label>{id} </label>
       <label>{name}</label>
       <Image src={imgSrc} width="128" height="128" alt={name} />
-      <input type="checkbox" checked={completed} onChange={changeCompleted} />
+      <input
+        type="checkbox"
+        checked={itemCompleted}
+        onChange={() => toggleCompleted(itemCompleted)}
+      />
     </div>
   );
 };
